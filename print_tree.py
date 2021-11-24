f9df606fdcc4bcd61f8a3ba0d71e24eb854ac9fe
Balint Kovari <balintmaci@gmail.com>
2021-11-24 11:55:50 +0100

    add functions for printing bullet and indent trees

--- print_tree.py
+++ print_tree.py
@@ -3,9 +3,40 @@ from tree import Node, Leaf, Visitor
 
 
 class PrintTree(Visitor):
+    BULLET = "*"
     def __init__(self, node_style) -> None:
+        self.node_style = node_style
         super().__init__()
 
+    def traverse(self, tree_element):
+        if self.node_style == NodeStyle.INDENT:
+            return self.traverse_indent(tree_element)
+        elif self.node_style == NodeStyle.BULLET:
+            return self.traverse_bullet(tree_element)
+        elif self.node_style == NodeStyle.TREE:
+            # return self.traverse_tree(tree_element)
+            return
+    
+    def traverse_indent(self, tree_element):
+        if (not isinstance(tree_element, Node)) and (not isinstance(tree_element, Leaf)):
+            raise TypeError()
+        children_part = ""
+        if isinstance(tree_element, Node) and tree_element.children:
+            for c in tree_element.children:
+                for line in self.traverse(c).splitlines():
+                    children_part += "\n  {}".format(line)
+        return "{}{}".format(tree_element.name, children_part)
+    
+    def traverse_bullet(self, tree_element):
+        if (not isinstance(tree_element, Node)) and (not isinstance(tree_element, Leaf)):
+            raise TypeError()
+        children_part = ""
+        if isinstance(tree_element, Node) and tree_element.children:
+            for c in tree_element.children:
+                for line in self.traverse(c).splitlines():
+                    children_part += "\n  {}".format(line)
+        return "{} {}{}".format(PrintTree.BULLET, tree_element.name, children_part)
+    
 @unique
 class NodeStyle(Enum):
     TREE = auto()
