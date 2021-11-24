e06a3128615df7a879817406baccb3bb1f6732d3
Balint Kovari <balintmaci@gmail.com>
2021-11-24 10:10:54 +0100

    implement basic functions in tree

--- tree.py
+++ tree.py
@@ -1,12 +1,29 @@
 
 
 class Leaf():
-    pass
+    def __init__(self, name: str):
+        self.name = name
+        self.parent = None
+
+    def accept(self):
+        pass
 
 
 class Node():
-    pass
+    def __init__(self, name: str, leaf1: Leaf, leaf2: Leaf):
+        self.name = name
+        self.parent = None
+        self.children = [leaf1, leaf2]
+        for child in self.children:
+            child.parent = self
+
+    def accept(self):
+        pass
 
 
 class Visitor():
-    pass
+    def traverse(self):
+        pass
+
+    def visit(self):
+        pass
